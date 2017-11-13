733c8de95f087c27d9b5adf9281432f85527d199
Charles <charles.porter@drake.edu>
2017-11-13 09:44:17 -0600

    update local

--- app/src/main/java/com/example/charles/project2/MainActivity.java
+++ app/src/main/java/com/example/charles/project2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.charles.project2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.addTab(tabLayout.newTab().setText("Featured"));
+        tabLayout.addTab(tabLayout.newTab().setText("Recipes"));
+        tabLayout.addTab(tabLayout.newTab().setText("Favorites"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
     }
 }
